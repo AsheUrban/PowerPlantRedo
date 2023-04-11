a9e75f55e5f237335a1816e7689340db81a809bc
Ashe Urban <ashe@goldentongue.com>
2023-04-11 10:57:22 -0700

    Set up Power Plant project.
    Co-Authored-By: Justin Lee <lee.justin001126@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
       verbose: true
     }),
     new HtmlWebpackPlugin({
-      title: 'JavaScript Template',
+      title: 'Power Plant',
       template: './src/index.html',
       inject: 'body'
     })
